365885ce73712752d7f9f55a3ea7560ae1de4a44
apacal <apacal@126.com>
2015-01-24 23:57:20 +0800

    fixbug

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG', true);
-define("SITE_URL", "http://apacal.cn");
+define("SITE_URL", "");
 
 // 定义应用目录
 define('APP_PATH','./Application/');
